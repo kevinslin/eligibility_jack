8182d3e693327dbedc02ab848b1aff532129470a
Kevin Lin <kevinslin8@gmail.com>
2013-03-06 14:37:03 -0600

    update points with random tie breaking

--- points.py
+++ points.py
@@ -1,4 +1,5 @@
 #!/usr/bin/envy python2.7
+import random
 
 POINTS_FILE = "sample.txt" # <Point, Name> pairs. See sample.txt for format
 POINTS_FILE_PROCESSED = "sample.out.txt" # Results will be stored in this file
@@ -6,13 +7,16 @@ POINTS_DESC = True # Sort order
 
 def points():
     """ sort a list of point name pairs """
+    def compare_with_ties(a, b):
+        diff = cmp(a, b)
+        return diff if diff else random.choice([-1,1])
     with open(POINTS_FILE) as fh:
         lines = fh.readlines()
         out = []
         for line in lines:
             u = line.split(" ")
             out.append((float(u[0]), (" ".join(u[1:]).strip())))
-        out = sorted(out, key=lambda x: x[0], reverse=POINTS_DESC)
+        out = sorted(out, key=lambda x: x[0], cmp=compare_with_ties, reverse=POINTS_DESC)
         with open(POINTS_FILE_PROCESSED, 'w') as fh:
             for line in out:
                 print(line)
